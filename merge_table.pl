f5aeb2399dc081aa3f0f185d93a2d0c00fbb0f57
Alexandre Lymberopoulos <lymber@ime.usp.br>
2013-04-18 18:27:59 -0300

    merge_table.pl: fixed bug related to students that didn't show up in the 		first exam. Need to modularize all the repeated code there.

--- merge_table.pl
+++ merge_table.pl
@@ -4,6 +4,60 @@ use strict;
 use warnings;
 use Term::ANSIColor;
 
+# First run (inicialization of the file)
+if ( $#ARGV == 2 ){
+# Year
+    my $ano = $ARGV[0];
+# Class Number
+    my $class = $ARGV[1];
+# Enrolled students
+    my $matriculados = "../dados/mat2457-$ano-matriculados-t$class.csv";
+    open(MATRICULADOS,"<", $matriculados) or die "Can't open $matriculados for reading: $!\n";
+# Tests grades already annouced
+    my $curr_tests = "./mat2457-$ano-t$class-$ARGV[2].dat";
+    open(CURRTESTS,"<", $curr_tests) or die "Can't open $curr_tests for reading: $!\n";
+# Output
+    my $output = "./mat2457-$ano-t$class-pronto-$ARGV[2].dat";
+    open(OUTPUT,">", $output) or die "Can't open $output for writing: $!\n";
+
+    my %hash_notas = ();
+    
+    while( $_ = <MATRICULADOS> ){
+	chomp($_);
+	# Remove quotes from name of student
+	$_ =~ s/"//g;
+	#student id
+	my $nusp = substr($_,0,7);
+	$hash_notas{$nusp} = "";
+    }
+    
+    close(MATRICULADOS);
+    
+    my %hash_divulgadas = ();
+    my $notas_divulgadas = "";
+    
+    while( $_ = <CURRTESTS> ){
+	chomp($_);
+	#student id
+	my $nusp = substr($_,0,7);
+	#student grades 
+	$notas_divulgadas = substr($_,,8);
+	$hash_divulgadas{$nusp} = $notas_divulgadas;
+    }
+    foreach (sort keys %hash_notas){
+	if ( !defined($hash_divulgadas{$_}) ){$hash_notas{$_}="- ";}
+	else{$hash_notas{$_}=$hash_divulgadas{$_};}
+    }
+    
+    select OUTPUT;
+    foreach (sort keys %hash_notas) {
+	print "$_ $hash_notas{$_}\n";
+    }
+    select STDOUT;
+    print color("green"), "Pronto! [Arquivo com as notas da primeira prova preparado.]\n", color("reset");
+    exit 0;
+}
+
 # Check correct call of program
 if ( $#ARGV != 3 || $ARGV[0] !~ /[0-9]{4}/ || $ARGV[1] !~ /0[1-9]{1}|1[0-3]{1}|20/) {
     print "Usage: $0 <ano> <turma> <current_tests> <newtest>\n";
@@ -20,14 +74,14 @@ my $matriculados = "../dados/mat2457-$ano-matriculados-t$class.csv";
 open(MATRICULADOS,"<", $matriculados) or die "Can't open $matriculados for reading: $!\n";
 
 # Tests grades already annouced
-my $curr_tests = "./mat2457-$ano-t$class-$ARGV[2].dat";
+my $curr_tests = "./mat2457-$ano-t$class-pronto-$ARGV[2].dat";
 open(CURRTESTS,"<", $curr_tests) or die "Can't open $curr_tests for reading: $!\n";
 # Test grades to be merged
 my $new_test = "./mat2457-$ano-t$class-$ARGV[3].dat";
 open(NEWTEST,"<", $new_test) or die "Can't open $new_test for reading: $!\n";
 
 # Output file
-my $output = "./mat2457-$ano-t$class-$ARGV[2]$ARGV[3].dat";
+my $output = "./mat2457-$ano-t$class-pronto-$ARGV[2]$ARGV[3].dat";
 open(OUTPUT,">",$output) or die "Can't open $output for reading: $!\n";
 
 my %hash_notas = ();
@@ -50,17 +104,12 @@ while( $_ = <CURRTESTS> ){
     chomp($_);
     #student id
     my $nusp = substr($_,0,7);
-    #student answers 
+    #student grades
     $notas_divulgadas = substr($_,,8);
     $hash_divulgadas{$nusp} = $notas_divulgadas;
 }
 
 close(CURRTESTS);
-
-print color("red"), "Notas já divulgadas\n", color("reset"); 
-foreach (sort keys %hash_divulgadas){
-    print "\t $_ $hash_divulgadas{$_}\n"
-}
 
 my %hash_novas = ();
 
@@ -68,29 +117,20 @@ while( $_ = <NEWTEST> ){
     chomp($_);
     #student id
     my $nusp = substr($_,0,7);
-    #student answers 
+    #student grades
     $hash_novas{$nusp} = substr($_,,8);
 }
 
 close(NEWTEST);
 
-print color("red"), "Notas novas\n", color("reset"); 
-foreach (sort keys %hash_novas){
-    print "\t $_ $hash_novas{$_}\n"
-}
-
 foreach (sort keys %hash_notas){
     if ( defined($hash_divulgadas{$_}) && defined($hash_novas{$_}) ){
-	$hash_notas{$_} = $hash_divulgadas{$_}.$hash_novas{$_}." ";
+	$hash_notas{$_} = $hash_divulgadas{$_}.$hash_novas{$_};
     }
     elsif (defined($hash_divulgadas{$_})){
-	$hash_notas{$_}=$hash_divulgadas{$_}."- "." ";
-	print "$_: Não veio na P2\n"
+	$hash_notas{$_}=$hash_divulgadas{$_}."- ";
     }
-    else {
-	$hash_notas{$_}="- - ";
-	print "$_: Nunca veio\n";
-    }
+    else {$hash_notas{$_}="- ";}
 }
 
 select OUTPUT;
